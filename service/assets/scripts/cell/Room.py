03cc35270f56341e38124a3fe9ae3c0ea49930de
sdkbb2007 <470557248@qq.com>
2018-08-20 12:30:51 +0800

    5.1-5.3
    5.1-5.3

--- service/assets/scripts/cell/Room.py
+++ service/assets/scripts/cell/Room.py
@@ -113,9 +113,265 @@ class Room(KBEngine.Entity):
 				seats[i].entity.client.game_begin_push()
 
 		print("游戏开始！");
+		##游戏开始检测
+		self.gameStartCheck(self.game)
+
+	def gameStartCheck(self,game):
+		for i in range(len(game.gameSeats)):
+			gs = game.gameSeats[i]
+			#检查是否有听牌
+			self.checkCanTingPai(game,gs)
+
+	#检查是否有听牌
+	def checkCanTingPai(self,game,seatData):
+		seatData.tingMap = {}
+		#检查手上的牌是不是已打缺，如果未打缺，则不进行判定
+		for i in range(len(seatData.holds)):
+			pai = seatData.holds[i]
+			if self.getMJType(pai) == seatData.que:
+				return
+
+		#检查是否是七对 前提是没有碰，也没有杠 ，即手上拥有13张牌
+		if len(seatData.holds) == 13:
+			pairCount = 0;
+			danPai = -1;
+			for k in seatData.countMap:
+				c = seatData.countMap.get(k)
+				if c == 2 or c == 3:
+					pairCount+=1
+				elif c == 4:
+					pairCount+=2
+				
+				if c == 1 or c == 3:
+					#如果已经有单牌了，表示不止一张单牌，并没有下叫。直接闪
+					if danPai>=0:
+						break
+
+					danPai = k
+
+			if pairCount == 6:
+				#七对只能和一张，就是手上那张单牌
+				seatData.tingMap[danPai] = {
+					"pattern":"7pairs",
+					"fan":"2",
+					}
+
+		#对对胡检测 由于四川麻将没有吃，所以只需要检查手上的牌
+		#对对胡叫牌有两种情况
+		#1、N坎 + 1张单牌
+		#2、N-1坎 + 两对牌
+		singleCount = 0
+		pairCount = 0
+		arr = []
+		for k in seatData.countMap:
+			c = seatData.countMap.get(k)
+			if c == 1:
+				singleCount+=1
+				arr.append(k)
+			elif c ==2:
+				pairCount+=1
+				arr.append(k)
+			elif c ==4:
+				singleCount+=1
+				pairCount+=2
+
+		if (pairCount == 0 and singleCount ==1) or (pairCount == 2 and singleCount == 0):
+			for i in range(len(arr)):
+				p = arr[i];
+				if seatData.tingMap.get(p,None) == None:
+					seatData.tingMap[p] ={
+						"pattern":"duidui",
+						"fan":"1"
+					}
+
+		#检查是不是平胡
+		#检查筒
+		if seatData.que !=0:
+			self.checkTingPai(seatData,0,9)
+		elif seatData.que !=1:
+			self.checkTingPai(seatData,9,18)
+		elif seatData.que !=2:
+			self.checkTingPai(seatData,18,27)
+
+	#检测听牌
+	def checkTingPai(self,seatData,begin,end):
+		for i in range(begin,end):
+			if seatData.tingMap.get(i,None) !=None:
+				continue
+			#将牌加入到计数中
+			old = seatData.countMap.get(i,None)
+			if old == None:
+				seatData.countMap[i] = 1
+			else:
+				seatData.countMap[i] += 1
+
+			seatData.holds.append(i)
+			#逐个判定手上的牌
+			ret = self.checkCanHu(seatData)
+			if ret:
+				#平胡 0番
+				seatData.tingMap[i] = {
+					"pattern":"normal",
+					"fan":"0"
+				}
+			#搞完以后，撤消刚刚加的牌
+			if old==None:
+				del seatData.countMap[i]
+			else:
+				seatData.countMap[i] = old
+			seatData.holds.pop()
+
+	##逐个判定手上的牌是否能让胡
+	def checkCanHu(self,seatData):
+		#如果当前牌大于等于２，则将它选为将牌
+		for k in seatData.countMap:
+			c = seatData.countMap.get(k)
+			if c == None:
+				c = 0
+			if c <2:
+				continue
+			seatData.countMap[k] -=2
+			#逐个判定剩下的牌是否满足　３Ｎ规则,一个牌会有以下几种情况
+			#1、0张，则不做任何处理
+			#2、2张，则只可能是与其它牌形成匹配关系
+			#3、3张，则可能是单张形成 A-2,A-1,A  A-1,A,A+1  A,A+1,A+2，也可能是直接成为一坎
+			#4、4张，则只可能是一坎+单张
+			ret = self.checkSingle(seatData)
+			seatData.countMap[k] +=2
+			if ret:
+				return True
 
 
+	#检查是不是单牌
+	def checkSingle(self,seatData):
+		holds = seatData.holds;
+		selected = -1;
+		c = 0
+		for i in range(len(holds)):
+			pai = holds[i]
+			c = seatData.countMap.get(pai)
+			if c !=0:
+				selected = pai
+				break
+		#如果没有找到剩余牌，则表示匹配成功了
+		if selected == -1:
+			return True
+		#否则，进行匹配
+		if c == 3:
+			#直接作为一坎
+			seatData.countMap[selected] = 0;
+			ret = self.checkSingle(seatData);
+			#立即恢复对数据的修改
+			seatData.countMap[selected] = c;
+			if ret ==True:
+				return True
+
+		elif c == 4:
+			#直接作为一坎
+			seatData.countMap[selected] = 1;
+			ret = self.checkSingle(seatData)
+			#立即恢复对数据的修改
+			seatData.countMap[selected] = c;
+			#如果作为一坎能够把牌匹配完，直接返回TRUE。
+			if ret == True:
+				return True
+
+		#按单牌处理
+		return self.matchSingle(seatData,selected)
+
+	#匹配单牌
+	def matchSingle(self,seatData,selected):
+		#分开匹配 A-2,A-1,A
+		matched = True
+		v = selected % 9
+		if v<2:
+			matched = False
+		else:
+			for i in range(3):
+				t = selected - 2 + i
+				cc = seatData.countMap.get(t)
+				if cc == None or cc == 0:
+					matched = False
+					break
 		
+		#匹配成功，扣除相应数值
+		if matched:
+			seatData.countMap[selected - 2] -=1
+			seatData.countMap[selected - 1] -=1
+			seatData.countMap[selected] -=1
+			ret = self.checkSingle(seatData);
+			seatData.countMap[selected - 2] +=1
+			seatData.countMap[selected - 1] +=1
+			seatData.countMap[selected] +=1
+			if ret == True:
+				return True
+
+		#分开匹配 A-1,A,A + 1
+		matched = True
+		if v<1 or v>7:
+			matched = False
+		else:
+			for i in range(3):
+				t = selected - 1 +i
+				cc = seatData.countMap.get(t)
+				if cc == None:
+					matched = False
+					break
+				if cc == 0:
+					matched = False
+					break
+		#匹配成功，扣除相应数值
+		if matched:
+			seatData.countMap[selected-1] -=1
+			seatData.countMap[selected] -=1
+			seatData.countMap[selected + 1] -=1
+			ret = self.checkSingle(seatData)
+			seatData.countMap[selected - 1] +=1
+			seatData.countMap[selected] +=1
+			seatData.countMap[selected + 1] +=1
+			if ret == True:
+				return True
+
+		#分开匹配 A,A+1,A + 2
+		matched = True
+		if v>6:
+			matched = False
+		else:
+			for i in range(3):
+				t = selected +i
+				cc = seatData.countMap.get(t)
+				if cc ==None:
+					matched = False
+					break
+				if cc == 0:
+					matched = False
+					break
+		#匹配成功，扣除相应数值
+		if matched:
+			seatData.countMap[selected] -=1
+			seatData.countMap[selected + 1] -=1
+			seatData.countMap[selected + 2] -=1
+			ret = self.checkSingle(seatData);
+			seatData.countMap[selected] +=1
+			seatData.countMap[selected + 1] +=1
+			seatData.countMap[selected + 2] +=1
+			if ret == True:
+				return True
+
+		return False
+
+	#获取麻将花色
+	def getMJType(self,pai):
+		if pai >= 0 and pai < 9:
+			#筒
+			return 0;
+		elif pai >= 9 and pai < 18:
+			#条
+			return 1;    
+		elif pai >= 18 and pai < 27:
+			#万
+			return 2;
+
 	#洗牌
 	def shuffle(self,game):
 		mahjongs = game.mahjongs
@@ -158,6 +414,15 @@ class Room(KBEngine.Entity):
 		pai = game.mahjongs[game.currentIndex]
 		game.gameSeats[seatIndex].holds.append(pai)
 		game.currentIndex +=1
+		data = game.gameSeats[seatIndex];
+		#统计牌的数目
+		c = data.countMap.get(pai,None)
+		if c== None:
+			c=0
+		data.countMap[pai] = c + 1;
+
+
+
 #----------------------------------------------------------------------------
 #麻将信息类
 class MJData:
@@ -183,8 +448,9 @@ class seatData:
 		self.userId = userId		#玩家id
 		self.holds = []  #持有的牌
 		#self.folds = []  #打出的牌
-
-
+		self.tingMap = {} #玩家手上的牌的数目，用于快速判定碰杠
+		self.countMap = {}  #玩家手上的牌的数目，用于快速判定碰杠
+		self.que = -1 #缺一门
 #房间信息
 class roomInfo:
 	def __init__(self,roomKey,maxPlayerCount):
